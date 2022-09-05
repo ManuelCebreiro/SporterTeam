776c71ca9708a84789efaa24d1937e756733cf96
dpizapal <dpizapal@yahoo.es>
2022-09-05 18:37:33 +0000

    foto de pertfil dentro de edituser

--- src/front/js/pages/perfil/edituser.js
+++ src/front/js/pages/perfil/edituser.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { Photoperfil } from "./photoperfil";
 
 import "../../../styles/register.css";
 
@@ -51,88 +52,90 @@ export const EditUser = () => {
   return (
     <div id="body_register">
       <form id="form_register" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="new_username">Username de {user.username}</label>
-        <input
-          name="new_username"
-          type="text"
-          placeholder="Tu nuevo Username"
-          className={errors.new_username && "error"}
-          {...register("new_username", {
-            minLength: {
-              value: 5,
-              message: "Username debe tener mínimo 5 caracteres",
-            },
-            pattern: {
-              value: patterns.new_username,
-              message: messages.new_username,
-            },
-          })}
-        />
-        {errors.new_username && <p>{errors.new_username.message}</p>}
+        <Photoperfil />
+        <div id="form_register_body">
+          <label htmlFor="new_username">Username de {user.username}</label>
+          <input
+            name="new_username"
+            type="text"
+            placeholder="Tu nuevo Username"
+            className={errors.new_username && "error"}
+            {...register("new_username", {
+              minLength: {
+                value: 5,
+                message: "Username debe tener mínimo 5 caracteres",
+              },
+              pattern: {
+                value: patterns.new_username,
+                message: messages.new_username,
+              },
+            })}
+          />
+          {errors.new_username && <p>{errors.new_username.message}</p>}
 
-        <label htmlFor="new_description">Descripción</label>
-        <textarea
-          id="description-textarea"
-          name="new_description"
-          type="text"
-          placeholder="Algo sobre tí"
-          className={errors.new_description && "error"}
-          {...register("new_description", {
-            maxLength: {
-              value: 300,
-              message: "Username debe tener máximo 300 caracteres",
-            },
-          })}
-        />
-        {errors.new_description && <p>{errors.new_description.message}</p>}
+          <label htmlFor="new_description">Descripción</label>
+          <textarea
+            id="description-textarea"
+            name="new_description"
+            type="text"
+            placeholder="Algo sobre tí"
+            className={errors.new_description && "error"}
+            {...register("new_description", {
+              maxLength: {
+                value: 300,
+                message: "Username debe tener máximo 300 caracteres",
+              },
+            })}
+          />
+          {errors.new_description && <p>{errors.new_description.message}</p>}
 
-        <label htmlFor="new_email">Email de {user.email}</label>
-        <input
-          name="new_email"
-          type="text"
-          placeholder="Tu nuevo correo electrónico"
-          className={errors.new_email && "error"}
-          {...register("new_email", {
-            pattern: {
-              value: patterns.new_email,
-              message: messages.new_email,
-            },
-          })}
-        />
-        {errors.new_email && <p>{errors.new_email.message}</p>}
+          <label htmlFor="new_email">Email de {user.email}</label>
+          <input
+            name="new_email"
+            type="text"
+            placeholder="Tu nuevo correo electrónico"
+            className={errors.new_email && "error"}
+            {...register("new_email", {
+              pattern: {
+                value: patterns.new_email,
+                message: messages.new_email,
+              },
+            })}
+          />
+          {errors.new_email && <p>{errors.new_email.message}</p>}
 
-        <label htmlFor="new_password">Contraseña nueva</label>
-        <input
-          name="new_password"
-          type="password"
-          className={errors.new_password && "error"}
-          {...register("new_password", {
-            minLength: {
-              value: 5,
-              message: "Contraseña debe tener mínimo 5 caracteres",
-            },
-          })}
-        />
-        {errors.new_password && <p>{errors.new_password.message}</p>}
+          <label htmlFor="new_password">Contraseña nueva</label>
+          <input
+            name="new_password"
+            type="password"
+            className={errors.new_password && "error"}
+            {...register("new_password", {
+              minLength: {
+                value: 5,
+                message: "Contraseña debe tener mínimo 5 caracteres",
+              },
+            })}
+          />
+          {errors.new_password && <p>{errors.new_password.message}</p>}
 
-        <label htmlFor="new_password_repeat">Repite contraseña nueva</label>
-        <input
-          name="new_password_repeat"
-          type="password"
-          className={errors.new_password_repeat && "error"}
-          {...register("new_password_repeatt", {
-            validate: (value) => {
-              if (watch("new_password") != value) {
-                return "Contraseña no coinciden";
-              }
-            },
-          })}
-        />
-        {errors.new_password_repeat && (
-          <p>{errors.new_password_repeat.message}</p>
-        )}
+          <label htmlFor="new_password_repeat">Repite contraseña nueva</label>
+          <input
+            name="new_password_repeat"
+            type="password"
+            className={errors.new_password_repeat && "error"}
+            {...register("new_password_repeatt", {
+              validate: (value) => {
+                if (watch("new_password") != value) {
+                  return "Contraseña no coinciden";
+                }
+              },
+            })}
+          />
+          {errors.new_password_repeat && (
+            <p>{errors.new_password_repeat.message}</p>
+          )}
 
-        {/* <label htmlFor="password">Confirmar con contraseña antigua</label>
+          {/* <label htmlFor="password">Confirmar con contraseña antigua</label>
         <input
           name="password"
           type="password"
@@ -148,21 +151,22 @@ export const EditUser = () => {
         />
         {errors.password && <p>{errors.password.message}</p>} */}
 
-        <label htmlFor="new_age">Edad: {user.age}</label>
-        <input
-          name="new_age"
-          placeholder="Actualizar edad"
-          className={errors.age && "error"}
-          {...register("new_age", {
-            pattern: {
-              value: patterns.new_age,
-              message: messages.new_age,
-            },
-          })}
-        />
-        {errors.new_age && <p>{errors.new_age.message}</p>}
+          <label htmlFor="new_age">Edad: {user.age}</label>
+          <input
+            name="new_age"
+            placeholder="Actualizar edad"
+            className={errors.age && "error"}
+            {...register("new_age", {
+              pattern: {
+                value: patterns.new_age,
+                message: messages.new_age,
+              },
+            })}
+          />
+          {errors.new_age && <p>{errors.new_age.message}</p>}
 
-        <input id="register_btn" value="submit" type="submit" />
+          <input id="register_btn" value="submit" type="submit" />
+        </div>
       </form>
       <Link className="fw-bold text-body" to="/home">
         Salir de Ajustes
