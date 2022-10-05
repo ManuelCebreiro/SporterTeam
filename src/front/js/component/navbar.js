9e03c55a092094891802743626431bef5d9e1490
ManuelCebreiro <cebreirom@gmail.com>
2022-10-05 08:19:45 +0000

    boton salir cambiado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -27,7 +27,7 @@ export const Navbarnew = () => {
         <Navbar.Collapse className="text-end" id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link to="/home">
-              <button id="btnnavbar-1" type="button" className="btn mt-1 ms-1">
+              <button id="btnnavbar-1" type="button" className="btn my-1 ms-1">
                 Principal
               </button>
             </Link>
@@ -36,7 +36,7 @@ export const Navbarnew = () => {
               <button
                 id="btnnavbar-2"
                 type="button"
-                className="atari btn mt-1 ms-1"
+                className="atari btn my-1 ms-1"
               >
                 <span>Crear Pachanga</span>
                 <div class="top"></div>
@@ -47,33 +47,17 @@ export const Navbarnew = () => {
             </Link>
           </Nav>
           <Link
-            id="btn-navbarexit-text"
-            className=""
             to="/"
             onClick={() => {
               actions.logout();
             }}
           >
-            Salir
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="35"
-              height="35"
-              fill="currentColor"
-              id="btn-navbarexit-icon"
-              class="bi bi-box-arrow-right "
-              aria-hidden="true"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-              />
-              <path
-                fill-rule="evenodd"
-                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-              />
-            </svg>
+            <img src="https://themayanagari.com/wp-content/uploads/2021/04/Exit-Button-Png-Icon-Transparent.png"
+              className="img-fluid rounded-circle my-1 float-end"
+
+              id="imagenExitNavbar"
+            />
+
             {/* <span
 
             id="btnnavbarexit"
